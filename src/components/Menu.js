939924722eb19ddcd3a24e3006858bd75239437e
dhruvinpatel1 <dhruvin@netfillip.com>
2023-07-28 15:33:12 +0530

    menu changes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import Link from 'next/link';
 import { ChevronDownIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
+import { useRouter } from 'next/router';
 
 export default function Menu({ menu }) {
+    const router = useRouter();
+    const pathname = router.pathname;
+    const mainmenutitle = pathname.split('/')[1];
+
 
     const [showSubMenu, setShowSubMenu] = useState(false);
     const [currentSubMenu, setCurrentSubMenu] = useState([]);
@@ -52,7 +57,9 @@ export default function Menu({ menu }) {
             <div className='flex'>
                 <div className='w-[16.66%] inline-block'>
                     <div className='p-[10px]'>
+                        <Link href="/">
                         <img src={`/bride-logo.png`} alt="logo" />
+                        </Link>
                     </div>
 
                 </div>
@@ -66,7 +73,7 @@ export default function Menu({ menu }) {
                                     onMouseLeave={() => { setShowSubMenu(false); setmenutitle(''); setShowSubSubMenu(false); setsubmenutitle(''); }}
                                     className="!relative py-[13px] px-[20px]"
                                 >
-                                    <Link href={menuItem.attributes.url} className={`text-[15px] uppercase font-semibold  ${(menuItem.attributes.title === menutitle || menuItem.attributes.title === "Home") ? "main_menu text-[#9E0851]" : 'text-[#767171]'} hover:after:content-[''] hover:after:absolute hover:after:bottom-[0px] hover:after:bg-[#9E0851] hover:after:w-full hover:after:h-[3px] hover:after:left-0 `} >{menuItem.attributes.title} {menuItem.attributes.children.data.length !== 0 && <ChevronDownIcon className="w-3 h-3 inline-block" />}</Link>
+                                    <Link href={menuItem.attributes.url} className={`text-[15px] uppercase font-semibold  ${(menuItem.attributes.title === menutitle || menuItem.attributes.title.toLowerCase() === (mainmenutitle == "" ? "home" : mainmenutitle)) ? "main_menu text-[#9E0851]" : 'text-[#767171]'} hover:after:content-[''] hover:after:absolute hover:after:bottom-[0px] hover:after:bg-[#9E0851] hover:after:w-full hover:after:h-[3px] hover:after:left-0 `} >{menuItem.attributes.title} {menuItem.attributes.children.data.length !== 0 && <ChevronDownIcon className="w-3 h-3 inline-block" />}</Link>
                                     {currentSubMenu.length !== 0 && showSubMenu && menuItem.attributes.title === menutitle && (
                                         <div id="submenuwidth" className="absolute left-0 top-[51px] bg-white z-20" onMouseLeave={() => { setShowSubSubMenu(false); setCurrentSubSubMenu([]); }}>
                                             <ul>
